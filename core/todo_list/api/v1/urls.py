93e4c306861bfd32aee7ca81c5b658bafeb80fa2
Arshia izadyar <110552657+arshiaa104@users.noreply.github.com>
2023-06-09 14:12:19 +0330

    On branch main

--- core/todo_list/api/v1/urls.py
+++ core/todo_list/api/v1/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
-from .views import TaskListAPIView, TaskRetrieve
+from django.urls import path, include
+from .views import TaskListAPIView, TaskRetrieve, TaskView
+from rest_framework.routers import DefaultRouter
 
+
+
+# task_list = TaskView.as_view({
+#     'get': "list"
+# })
+
+router = DefaultRouter()
+router.register('list', TaskView,basename="snippet")
 
 urlpatterns = [
-    path('task-list/', TaskListAPIView.as_view(), name='task-list'),
-    path('task-detail/<int:pk>/', TaskRetrieve.as_view(), name='task-detail'),
+    # path('task-list/', task_list, name='task-list'),
+    # path('task-detail/<int:pk>/', TaskRetrieve.as_view(), name='task-detail'),
+    path('', include(router.urls)),
 ]
--- core/todo_list/api/v1/views.py
+++ core/todo_list/api/v1/views.py
@@ -1,5 +1,8 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from todo_list.models import TaskModel
 from .serializer import TaskSerializer
 
@@ -27,3 +30,29 @@ class TaskRetrieve(RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(user=self.request.user)
+
+
+class TaskView(ViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = TaskModel.objects.all()
+    serializer_class = TaskSerializer
+    
+    def list(self, request):
+        # query = 
+        serializer = TaskSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
